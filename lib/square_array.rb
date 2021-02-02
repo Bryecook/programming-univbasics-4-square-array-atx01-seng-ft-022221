d11ab1aeffdd6543662898509341b786f840a8fe
Brian Cook <brybcook@gmail.com>
2021-02-02 17:19:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array=[]
   square_array=[]
-  while array.length.times do |index|
+  array.length.times do |index|
     square_array.push(array[index]*array[index])
   end
   square_array
